24008178069abcceb6437bd8d4743b11ff416345
martin <eqmaz@hotmail.com>
2025-04-10 16:08:55 +0100

    + prelude

---
+++ crates/trade_core/src/prelude.rs
@@ -0,0 +1,4 @@
+pub use crate::engine::TradeEngine;
+pub use crate::errors::ErrCodes as TradeErrors;
+pub use crate::model::{Currency, Direction, Trade, TradeDetails, TradeId};
+pub use crate::store::{InMemoryStore, TradeStore};
